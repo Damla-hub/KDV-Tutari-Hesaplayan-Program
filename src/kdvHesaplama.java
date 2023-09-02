1c2823c08a3e0f2f1fd2c73e4195a5cc8ff55da6
Damla Kilinc <dddmlklnc@gmail.com>
2023-09-02 18:44:36 +0300

    first commit

---
+++ src/kdvHesaplama.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class kdvHesaplama {
+    public static void main(String[] args) {
+        double fiyat,kdvFiyat,kdvHesap;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Fiyat değerini giriniz: ");
+        fiyat=input.nextDouble();
+        double kdv =fiyat<1000 ? 0.08 : 0.18;
+        kdvHesap=fiyat*kdv;
+        kdvFiyat=kdvHesap+fiyat;
+        System.out.print("KDV'siz Fiyat: "+fiyat+"\nKDV'li Fiyat: "+kdvFiyat+"\nKDV tutarı: "+kdvHesap);
+
+    }
+
+}
