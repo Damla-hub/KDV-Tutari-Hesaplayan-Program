46305540f8a70f98667cf412d51076f3bf3749ee
Damla-hub <143180878+Damla-hub@users.noreply.github.com>
2023-09-08 11:00:52 +0300

    Update kdvHesaplama.java

--- src/kdvHesaplama.java
+++ src/kdvHesaplama.java
@@ -6,7 +6,7 @@ public class kdvHesaplama {
         Scanner input=new Scanner(System.in);
         System.out.print("Fiyat değerini giriniz: ");
         fiyat=input.nextDouble();
-        double kdv =fiyat<1000 ? 0.08 : 0.18;
+        double kdv =fiyat<1000 ? 0.18 : 0.08;
         kdvHesap=fiyat*kdv;
         kdvFiyat=kdvHesap+fiyat;
         System.out.print("KDV'siz Fiyat: "+fiyat+"\nKDV'li Fiyat: "+kdvFiyat+"\nKDV tutarı: "+kdvHesap);
